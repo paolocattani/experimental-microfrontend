43e1dff727f393b1442c7281eab78908479c73d7
Paolo Cattani <p.cattani@tecmasolutions.com>
2022-03-31 16:32:15 +0200

    first commit : basic example

---
+++ app1/src/index.js
@@ -0,0 +1,5 @@
+// You can write your own logic here to determine the actual url
+//window.app2Url = "https://frbbqyjw1ptv.compat.objectstorage.eu-frankfurt-1.oraclecloud.com/tecma-assets-coll/experimental_mf/app2"
+window.app2Url = 'http://localhost:3000/static/';
+// Use dynamic import here to allow webpack to interface with module federation code
+import("./bootstrap");
